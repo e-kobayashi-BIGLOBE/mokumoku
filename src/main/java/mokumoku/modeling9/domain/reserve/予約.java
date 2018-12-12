d1054b3f42c54bf59c3af0e7d60663714b20f3f3
e-kobayashi <e-kobayashi@biglobe.co.jp>
2018-12-12 20:48:22 +0900

    重複チェックを予約リストに移動、重複リストを予約リストで作る

--- src/main/java/mokumoku/modeling9/domain/reserve/予約.java
+++ src/main/java/mokumoku/modeling9/domain/reserve/予約.java
@@ -1,6 +1,5 @@
 package mokumoku.modeling9.domain.reserve;
 
-import javaslang.control.Either;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,12 +15,7 @@ public class 予約 {
     private final 予約者 _予約者;
     private final 会議室 _会議室;
 
-    public 予約結果 getResult(予約リスト list) {
-        重複予約リスト dupList = new 重複予約リスト(
-                list.getList().filter(v -> v.get_予約時間().isかぶってる(_予約時間))
-        );
-        return dupList.isEmpty()
-                ? new 予約結果(Either.right(new 予約結果OK(this)))
-                : new 予約結果(Either.left(new 予約結果NG(dupList)));
+    public boolean isかぶってる(予約時間 _new予約時間) {
+        return _予約時間.isかぶってる(_new予約時間);
     }
 }
--- src/main/java/mokumoku/modeling9/domain/reserve/予約リスト.java
+++ src/main/java/mokumoku/modeling9/domain/reserve/予約リスト.java
@@ -1,6 +1,7 @@
 package mokumoku.modeling9.domain.reserve;
 
 import javaslang.collection.List;
+import javaslang.control.Either;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,4 +13,13 @@ import lombok.ToString;
 public class 予約リスト {
     @Getter
     private final List<予約> list;
+
+    public 予約結果 重複チェック(予約 _予約) {
+        重複予約リスト dupList = new 重複予約リスト(
+                list.filter(v -> v.isかぶってる(_予約.get_予約時間()))
+        );
+        return dupList.isEmpty()
+                ? new 予約結果(Either.right(new 予約結果OK(_予約)))
+                : new 予約結果(Either.left(new 予約結果NG(dupList)));
+    }
 }
